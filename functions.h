a1d40f8b9ff63cd5962b08b87032f6f018362654
Alterise <a.tereshkov@sbergames.ru>
2022-02-17 18:37:39 +0300

    fix functions.h

--- functions.h
+++ functions.h
@@ -3,45 +3,41 @@
 #include <chrono>
 #include "utils.h"
 
-auto increment = [](int val) {
+int increment(int val) {
     return val + 1;
-};
-
-// auto string_argument = [](std::string s1, std::string s2) {
-//     auto s = s1[0] + s2[0];
-// };
+}
 
 void string_argument(std::string s1, std::string s2) {
     auto s = s1[0] + s2[0];
 }
 
-auto string_const_argument = [](const std::string &s1, const std::string &s2) {
+void string_const_argument(const std::string &s1, const std::string &s2) {
     auto s = s1[0] + s2[0];
-};
+}
 
-auto string_view_argument = [](const std::string_view &s1, const std::string_view &s2) {
+void string_view_argument(const std::string_view &s1, const std::string_view &s2) {
     auto s = s1[0] + s2[0];
-};
+}
 
-auto string_char_argument = [](const char *s1, const char *s2) {
+void string_char_argument(const char *s1, const char *s2) {
     auto s = s1[0] + s2[0];
-};
+}
 
-auto string_iterator = [](std::string s1, std::string s2, int count) {
+std::string string_iterator(std::string s1, std::string s2, int count) {
     return (count % 2 ? s1 : s2);
-};
+}
 
-auto string_const_iterator = [](const std::string &s1, const std::string &s2, int count) {
+std::string string_const_iterator(const std::string &s1, const std::string &s2, int count) {
     return (count % 2 ? s1 : s2);
-};
+}
 
-auto string_view_iterator = [](const std::string_view &s1, const std::string_view &s2, int count) {
+std::string_view string_view_iterator(const std::string_view &s1, const std::string_view &s2, int count) {
     return (count % 2 ? s1 : s2);
-};
+}
 
-auto string_char_iterator = [](const char *s1, const char *s2, int count) {
+std::string string_char_iterator(const char *s1, const char *s2, int count) {
     return (count % 2 ? s1 : s2);
-};
+}
 
 template<typename T>
 void printElapsedTime(const std::string &name, T s, T e, bool double_el = true) {
